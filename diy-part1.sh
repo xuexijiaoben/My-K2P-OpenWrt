0edb84665238a5301a844151687f3004d1a52d9d
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2022-11-12 20:53:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@
 
 rm -rf package/lean/luci-app-ikoolproxy
 rm -rf package/lean/luci-app-ikoolproxy2
-git clone https://github.com/ilxp/luci-app-ikoolproxy2.git package/lean/luci-app-ikoolproxy
+git clone https://github.com/ilxp/luci-app-ikoolproxy.git package/lean/luci-app-ikoolproxy
 
 # rm -rf package/feeds/luci/luci-theme-argon
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
