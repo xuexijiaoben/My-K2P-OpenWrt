872971f7762795853c0514448960e5f9fee55ad2
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2024-02-01 20:38:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+wget https://github.com/coolsnowwolf/lede/files/14081054/2023.3-batman-adv.tar.gz
+rm -rf feeds/routing/batman-adv
+tar -zxvf 2023.3-batman-adv.tar.gz -C feeds/routing/
+rm -rf 2023.3-batman-adv.tar.gz
+
 # 1.修改默认ip
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
